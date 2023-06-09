4f907c89bbd486994a8db230ca7363d0f47a9483
izotov_a <aleksandr.izotov2020@gmail.com>
2023-06-09 08:56:15 +0300

    Initial commit

---
+++ src/main/java/com/assortment/sheet/AssortmentSheetApplication.java
@@ -0,0 +1,12 @@
+package com.assortment.sheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssortmentSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssortmentSheetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/assortment/sheet/AssortmentSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assortment.sheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssortmentSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
